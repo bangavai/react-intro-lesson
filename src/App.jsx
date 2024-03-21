ea17a579409441633f8f4f9fc4cebee1d452d383
bangavai <banga.vaitkute@gmail.com>
2024-03-21 20:54:15 +0200

    lift state to create counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-
 import Button from "./components/Button";
 import "./App.css";
-
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
 
-  const onBlurEvent = (e) => { console.log(e); }
-  
-  
-    return (
-      <div className="container">
-        <img
-          src="https://www.w3schools.com/howto/img_avatar.png"
-          alt="avatar"
-          className="avatar"
-        />
-        <h1>Welcome to react!</h1>
-        <Button text="Add" />
-        <Button text="Subtract" />
-        <input onBlur={onBlurEvent} />
-      </div>
-    );
-  }
-  
-  export default App;
+  const handleAdd = () => setCount(count + 1);
+  const handleSubtract = () => setCount(count - 1);
+
+  return (
+    <div className="container">
+      <img
+        src="https://www.w3schools.com/howto/img_avatar.png"
+        alt="avatar"
+        className="avatar"
+      />
+      <h1>Welcome to react!</h1>
+      <Button text="Add" onClicking={handleAdd} />
+      <Button text="Subtract" onClicking={handleSubtract} />
+      <input
+        value={count}
+        style={{
+          width: "290px",
+          height: "60px",
+          background: "bisque",
+          border: "2px solid black",
+          borderRadius: "5px",
+          fontSize: "30px",
+          textAlign: "center",
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
 import "./Button.css";
 
 const Button = (props) => {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
   return (
-    <button className="custom-button" onClick={handleClick}>
-      {props.text} {count}
+    <button className="custom-button" onClick={props.onClicking}>
+      {props.text}
     </button>
   );
 };
 
 export default Button;
+
